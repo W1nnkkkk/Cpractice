f54101ea456ba48efa0e62268e0b2ba46cf3d374
W1nnkkkk <tivchenko5442@gmail.com>
2024-02-18 12:55:29 +0300

    сделал 15 практику

---
+++ practic15/students.c
@@ -0,0 +1,71 @@
+#include "students.h"
+
+static int i = 0;
+
+void *stud_input(void *stud)
+{
+  struct students *tmp = stud;
+
+  printf("Для начала введите анкетную информацию о студенте: ");
+  printf("Фамилия, Имя: ");
+  scanf("%s %s", tmp->family, tmp->name);
+  printf("Пол, группа: ");
+  scanf("%s %s", tmp->gender, tmp->group);
+  printf("Возраст и оценки по математике, химии и физике: ");
+  scanf("%d %d %d %d", &tmp->age, &tmp->mathMark, &tmp->chemistryMark, &tmp->physicsMark);
+
+  FILE *file;
+
+  file = fopen("test.txt", "a");
+
+  fwrite(tmp, 1, sizeof(*tmp), file);
+
+  fclose(file);
+
+  return tmp;
+}
+
+void *stud_print(void *stud)
+{
+  struct students *tmp = stud;
+  printf("Анкетные данные студента: ");
+  printf(" Фамилия, Имя: ");
+  printf("%s %s", tmp->family, tmp->name);
+  printf(" Пол, группа: ");
+  printf("%s %s", tmp->gender, tmp->group);
+  printf(" Возраст и оценки по математике, химии и физике: ");
+  printf("%d %d %d %d", tmp->age, tmp->mathMark, tmp->chemistryMark, tmp->physicsMark);
+  printf("\n");
+  return NULL;
+}
+
+void *stud_init(void *stud)
+{
+  stud = (struct students *)malloc(sizeof(struct students));
+  struct students *tmp = stud;
+
+  tmp->input = stud_input;
+  tmp->print = stud_print;
+
+  return tmp;
+}
+
+void *studReadFromFile(void *stud)
+{
+  struct students *tmp = stud;
+
+  FILE *f;
+
+  f = fopen("test.txt", "r");
+
+  ++i;
+
+  for (size_t s = 0; s < i; s++)
+  {
+    fread(tmp, 1, sizeof(*tmp), f);
+  }
+
+  fclose(f);
+
+  return tmp;
+}
---
+++ practic15/students.h
@@ -0,0 +1,27 @@
+#ifndef STUDENTS_H
+#define STUDENTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PATH "test.txt"
+
+typedef struct students
+{
+  char family[20], name[20], gender[2], group[7];
+  int age, mathMark, physicsMark, chemistryMark;
+
+  void *(*input)(void *);
+  void *(*print)(void *);
+} Students;
+
+void *stud_input(void *stud);
+
+void *stud_print(void *stud);
+
+void *stud_init(void *stud);
+
+void *studReadFromFile(void *stud);
+
+#endif // STUDENTS_H
